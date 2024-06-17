c49950049a07644921160c06ac4d898333837f47
truongtdz <truongtd0910@gmail.com>
2024-06-17 20:45:51 +0700

    bai10

---
+++ bai810.java
@@ -0,0 +1,22 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class bai810 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            String tmp = sc.next();
+            if(map.containsKey(tmp)){
+                map.put(tmp, map.get(tmp) + 1);
+            } else 
+                map.put(tmp, 1);
+        }
+        for(Map.Entry<String, Integer> x : map.entrySet()){
+            System.out.println(x.getKey() + " " + x.getValue());
+        }
+        sc.close();
+    }
+}
