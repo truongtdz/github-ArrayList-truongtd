cb0a412d72768404b18758923d5d138c99497863
truongtdz <truongtd0910@gmail.com>
2024-06-17 22:50:19 +0700

    bai 13

--- bai813.java
+++ bai813.java
@@ -1,51 +1,28 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class bai813 {
-    public static ArrayList<ArrayList<Integer>> nhap(ArrayList<ArrayList<Integer>> list, int n, int m){
-        Scanner sc = new Scanner(System.in);
-        ArrayList<ArrayList<Integer>> tmp = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            ArrayList<Integer> arr = new ArrayList<>();
-            for(int j = 0; j < m; j++){
-                int x = sc.nextInt();
-                arr.add(x);
-            }
-            tmp.add(arr);
-        }
-        sc.close();
-        return tmp;
-    }
-
-    public static void xuat(ArrayList<ArrayList<Integer>> list, int n, int m){
-        for(int i = 0; i < n; i++){
-            System.out.println();
-            for(int j = 0; j < m; j++){
-                System.out.print(list.get(i).get(j) + " ");
-            }
-        }
-    }
-
-    public static ArrayList<ArrayList<Integer>> latNguoc(ArrayList<ArrayList<Integer>> list, int n, int m){
-        ArrayList<ArrayList<Integer>> tmp = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            ArrayList<Integer> arr = new ArrayList<>();
-            for(int j = 0; j < m; j++){
-                arr.add(list.get(i).get(m - j - 1));
-            }
-            tmp.add(arr);
-        }
-        return tmp;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        list = nhap(list, n, m);
-        list = latNguoc(list,n , m);
-        xuat(list, n, m);
+        for(int i = 0; i < n; i++){
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int j = 0; j < m; j++){
+                arr.add(sc.nextInt());
+            }
+            list.add(arr);
+        }
+        for(ArrayList<Integer> x : list){
+            Collections.reverse(x);
+        }
+        for(int i = 0; i < n; i++){
+            System.out.println();
+            for(Integer x : list.get(i)){
+                System.out.print(x + " ");
+            }
+        }
         sc.close();
     }
 }
